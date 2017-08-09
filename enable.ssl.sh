58c9f104bff31240a77256550b6be5fd3bb5d48a
Artem Pikulin <ortgma@gmail.com>
2017-08-09 16:21:57 +0700

    Closes #1. Add ssl configs and separate Docker image for landings.

---
+++ enable.ssl.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+certbot certonly --webroot -w /var/apps/acme -d jincor.com -d www.jincor.com -d ico.jincor.com -d api.jincor.com -m tech@jincor.com -n --agree-tos
+
+rm /etc/nginx/sites-available/api.jincor.com.conf
+rm /etc/nginx/sites-available/ico.jincor.com.conf
+rm /etc/nginx/sites-available/www.jincor.com.conf
+rm /etc/nginx/sites-available/jincor.com.conf
+
+ln -s /etc/nginx/ssl/api.jincor.com.ssl.conf /etc/nginx/sites-available/api.jincor.com.ssl.conf
+ln -s /etc/nginx/ssl/ico.jincor.com.ssl.conf /etc/nginx/sites-available/ico.jincor.com.ssl.conf
+ln -s /etc/nginx/ssl/www.jincor.com.ssl.conf /etc/nginx/sites-available/www.jincor.com.ssl.conf
+ln -s /etc/nginx/ssl/jincor.com.ssl.conf /etc/nginx/sites-available/jincor.com.ssl.conf
+nginx -s reload
