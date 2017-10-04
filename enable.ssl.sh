b212a45b41e19c61520c47084528d12919414dda
Artem Pikulin <ortgma@gmail.com>
2017-10-04 23:45:02 +0700

    Closes #3. Update jincor.com.inc.conf with new prod version.

--- enable.ssl.sh
+++ enable.ssl.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-certbot certonly --webroot -w /var/apps/acme -d jincor.com -d www.jincor.com -d ico.jincor.com -d api.jincor.com -m tech@jincor.com -n --agree-tos
+certbot certonly --webroot -w /var/apps/acme -d jincor.com -d www.jincor.com -d api.jincor.com -m tech@jincor.com -n --agree-tos
 
 rm /etc/nginx/sites-available/api.jincor.com.conf
-rm /etc/nginx/sites-available/ico.jincor.com.conf
 rm /etc/nginx/sites-available/www.jincor.com.conf
 rm /etc/nginx/sites-available/jincor.com.conf
 
 ln -s /etc/nginx/ssl/api.jincor.com.ssl.conf /etc/nginx/sites-available/api.jincor.com.ssl.conf
-ln -s /etc/nginx/ssl/ico.jincor.com.ssl.conf /etc/nginx/sites-available/ico.jincor.com.ssl.conf
 ln -s /etc/nginx/ssl/www.jincor.com.ssl.conf /etc/nginx/sites-available/www.jincor.com.ssl.conf
 ln -s /etc/nginx/ssl/jincor.com.ssl.conf /etc/nginx/sites-available/jincor.com.ssl.conf
 nginx -s reload
